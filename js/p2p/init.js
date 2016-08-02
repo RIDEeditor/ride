9cdb83d6691bcff220a08ebe9e1c7a29c3729edd
Nicholas <nick.a.molloy@gmail.com>
2016-08-02 18:56:25 +1200

    Allow the user to switch between sharing their webcam and their screen

--- js/p2p/init.js
+++ js/p2p/init.js
@@ -1,109 +1,138 @@
+"use strict";
+
 const Peer = require("peerjs");
+const {desktopCapturer} = require("electron");
 const Handlebars = require("handlebars");
-$(function(){
 
+$(function() {
+  var isCameraInput = true;
   var messages = [];
-  var peer_id, name, conn;
-  var messages_template = Handlebars.compile($('#messages-template').html());
+  var peerID;
+  var name;
+  var conn;
+  var messagesTemplate = Handlebars.compile($("#messages-template").html());
 
-  var peer = new Peer({key: "lwjd5qra8257b9", debug: 3});
+  var peer = new Peer({key: "lwjd5qra8257b9"});
 
-  peer.on('open', function(){
-    $('#id').text(peer.id);
+  peer.on("open", function() {
+    $("#id").text(peer.id);
   });
 
-  navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
-
-  function getVideo(callback){
-    navigator.getUserMedia({audio: true, video: true}, callback, function(error){
-      console.log(error);
-      alert('An error occured. Please try again');
-    });
+  function onGetUserMediaError(error) {
+    console.log(error);
+    alert("An error occured. Please try again");
   }
 
-  getVideo(function(stream){
+  function onGetUserMedia(stream) {
     window.localStream = stream;
-    onReceiveStream(stream, 'my-camera');
-  });
-
-  function onReceiveStream(stream, element_id){
-    var video = $('#' + element_id + ' video')[0];
-    video.src = window.URL.createObjectURL(stream);
-    window.peer_stream = stream;
+    onReceiveStream(stream, "my-camera");
   }
 
-  $('#login').click(function(){
-    name = $('#name').val();
-    peer_id = $('#peer_id').val();
-    if(peer_id){
-      conn = peer.connect(peer_id, {metadata: {
-        'username': name
+  navigator.webkitGetUserMedia({audio: true, video: true}, onGetUserMedia, onGetUserMediaError);
+
+  function toggleMediaInput() {
+    if (isCameraInput) {
+      // Switch to screen input
+      desktopCapturer.getSources({types: ["window"]}, function(error, sources) {
+        if (error) throw error;
+        for (let i = 0; i < sources.length; ++i) {
+          console.log(sources[i]);
+          if (sources[i].name === "RIDE Editor") {
+            navigator.webkitGetUserMedia({
+              audio: false,
+              video: {
+                mandatory: {
+                  chromeMediaSource: "desktop",
+                  chromeMediaSourceId: sources[i].id
+                }
+              }
+            }, onGetUserMedia, onGetUserMediaError);
+          }
+        }
+      });
+      isCameraInput = false;
+    } else {
+      // Switch to camera input
+      navigator.webkitGetUserMedia({audio: true, video: true}, onGetUserMedia, onGetUserMediaError);
+      isCameraInput = true;
+    }
+  }
+
+  $("#switch-input").on("click", toggleMediaInput);
+
+  function onReceiveStream(stream, elementID) {
+    var video = $("#" + elementID + " video")[0];
+    video.src = window.URL.createObjectURL(stream);
+    window.peerStream = stream;
+  }
+
+  $("#login").click(function() {
+    name = $("#name").val();
+    peerID = $("#peer_id").val();
+    if (peerID) {
+      conn = peer.connect(peerID, {metadata: {
+        username: name
       }});
-      conn.on('data', handleMessage);
+      conn.on("data", handleMessage);
     }
 
-    $('#chat').removeClass('hidden');
-    $('#connect').addClass('hidden');
+    $("#chat").removeClass("hidden");
+    $("#connect").addClass("hidden");
   });
 
-  peer.on('connection', function(connection){
+  peer.on("connection", function(connection) {
     conn = connection;
-    peer_id = connection.peer;
-    conn.on('data', handleMessage);
+    peerID = connection.peer;
+    conn.on("data", handleMessage);
 
-    $('#peer_id').addClass('hidden').val(peer_id);
-    $('#connected_peer_container').removeClass('hidden');
-    $('#connected_peer').text(connection.metadata.username);
+    $("#peer_id").addClass("hidden").val(peerID);
+    $("#connected_peer_container").removeClass("hidden");
+    $("#connected_peer").text(connection.metadata.username);
   });
 
-  function handleMessage(data){
-    var header_plus_footer_height = 285;
+  function handleMessage(data) {
     messages.push(data);
-
-    var html = messages_template({'messages' : messages});
-    $('#messages').html(html);
-
+    var html = messagesTemplate({messages: messages});
+    $("#messages").html(html);
     $("#messages-container").animate({scrollTop: $(document).height()}, 0);
-
   }
 
-  function sendMessage(){
-    var text = $('#message').val();
-    var data = {'from': name, 'text': text};
+  function sendMessage() {
+    var text = $("#message").val();
+    var data = {from: name, text: text};
 
     conn.send(data);
     handleMessage(data);
-    $('#message').val('');
+    $("#message").val("");
   }
 
-  $('#message').keypress(function(e){
-    if(e.which == 13){
+  $("#message").keypress(function(e) {
+    if (e.which === 13) {
       sendMessage();
     }
   });
 
-  $('#send-message').click(sendMessage);
+  $("#send-message").click(sendMessage);
 
-  $('#call').click(function(){
-    console.log('now calling: ' + peer_id);
+  $("#call").click(function() {
+    console.log("now calling: " + peerID);
     console.log(peer);
-    var call = peer.call(peer_id, window.localStream);
-    call.on('stream', function(stream){
-      window.peer_stream = stream;
-      onReceiveStream(stream, 'peer-camera');
+    var call = peer.call(peerID, window.localStream);
+    call.on("stream", function(stream) {
+      window.peerStream = stream;
+      onReceiveStream(stream, "peer-camera");
     });
   });
 
-  peer.on('call', function(call){
+  peer.on("call", function(call) {
     onReceiveCall(call);
   });
 
-  function onReceiveCall(call){
+  function onReceiveCall(call) {
     call.answer(window.localStream);
-    call.on('stream', function(stream){
-      window.peer_stream = stream;
-      onReceiveStream(stream, 'peer-camera');
+    call.on("stream", function(stream) {
+      window.peerStream = stream;
+      onReceiveStream(stream, "peer-camera");
     });
   }
-
 });
