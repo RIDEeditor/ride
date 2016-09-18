2d86ee775ec00e8d00eca593702be1dbf2e61967
Nicholas Molloy <nick.a.molloy@gmail.com>
2016-09-18 21:45:39 +1200

    Need to initalise visualisation dialog at startup or it will break page layout.

--- js/init.js
+++ js/init.js
@@ -171,6 +171,8 @@ $(window).load(function() {
   // setup bundle dialog
   $("#create-bundle-dialog").dialog({autoOpen: false, title: "Bundle Project", height: 200, width: 600,resizable:false});
 
+  // Setup visualisation dialog
+  $("#visualisation-dialog").dialog({autoOpen: false, title: "Generate Model", height: 500, width: 600,resizable:false});
   // Setup hide/show for options in diagram dialog
   $(function() {
     let acc = $(".accordion")[0];
